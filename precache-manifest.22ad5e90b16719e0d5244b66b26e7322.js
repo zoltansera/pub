f3e5dba005ff91ea7f8b3c59fd1266c32faf3d62
zoltansera <dev.zoltan.sera@gmail.com>
2021-10-19 15:03:07 +0200

    Updates

---
+++ precache-manifest.22ad5e90b16719e0d5244b66b26e7322.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82a08bd0ca30d915b2a71c1f587e89cf",
+    "url": "/pub/index.html"
+  },
+  {
+    "revision": "9120dfeedcf8e3ac203c",
+    "url": "/pub/static/css/main.cd6e2383.chunk.css"
+  },
+  {
+    "revision": "a43f978392a25df6bc0a",
+    "url": "/pub/static/js/2.43cd2162.chunk.js"
+  },
+  {
+    "revision": "448365ce1985613b6bc3197197352a0c",
+    "url": "/pub/static/js/2.43cd2162.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6401b6afeece9ddd270",
+    "url": "/pub/static/js/3.8e796bfc.chunk.js"
+  },
+  {
+    "revision": "9120dfeedcf8e3ac203c",
+    "url": "/pub/static/js/main.b79e89f3.chunk.js"
+  },
+  {
+    "revision": "9512c125f09c2e1d408c",
+    "url": "/pub/static/js/runtime-main.88cf3b4d.js"
+  },
+  {
+    "revision": "50aca90b4e0d025e94893c1de39800d3",
+    "url": "/pub/static/media/bar.50aca90b.jpg"
+  },
+  {
+    "revision": "0896a2cbc9b900324d9fe49430fcf692",
+    "url": "/pub/static/media/bar2.0896a2cb.jpg"
+  },
+  {
+    "revision": "fd3f29e70f245170814767203fe44d8e",
+    "url": "/pub/static/media/bar3.fd3f29e7.jpg"
+  },
+  {
+    "revision": "fdab971c889b801e9e54a0e4bcaa2e11",
+    "url": "/pub/static/media/bar4.fdab971c.jpg"
+  },
+  {
+    "revision": "852d71df8be2e3d2802b3c073c8fd628",
+    "url": "/pub/static/media/comedy.852d71df.jpg"
+  },
+  {
+    "revision": "c814c6fec2cd42a40faebc249258d5a4",
+    "url": "/pub/static/media/koncert.c814c6fe.jpg"
+  },
+  {
+    "revision": "056ed3d7f56a3f80e1c8dbaf7b73d47f",
+    "url": "/pub/static/media/koncert2.056ed3d7.jpg"
+  },
+  {
+    "revision": "89702d61bf192588b4e277579c2509b6",
+    "url": "/pub/static/media/map.89702d61.jpg"
+  }
+]);
